0f77f41e39b1b3d3578136d56061738ce600f7b6
bayutresna <bayutresna2000@gmail.com>
2024-02-02 18:12:03 +0700

    add support for bulk upload and bulk delete

--- upload.py
+++ upload.py
@@ -1,21 +1,26 @@
 from fastapi import FastAPI, UploadFile, File
+from typing import List
 import os
 import shutil
 app = FastAPI() 
 
 db_directory = "./db/"
 cache_directory = "./__pycache__/"
+document_directory = "./files/"
 
 @app.post("/")
-async def upload(file:UploadFile = File(...)):
-    file_path = f"files/{file.filename}"
-    with open(file_path, "wb") as f:
-        content = await file.read()
-        f.write(content)
-    return{"success":True, "file_path":file_path, "message":"file uploaded successfully"}
+async def upload(files:List[UploadFile] = File(...)):
+    if not os.path.exists('files'):
+        os.mkdir('files')
+    for file in files:
+        file_path = f"files/{file.filename}"
+        with open(file_path, "wb") as f:
+            content = await file.read()
+            f.write(content)
+    return{"success":True, "message":"files has uploaded successfully"}
 
 @app.get("/reset-db")
-async def reset():
+async def resetdb():
     
     for name in os.listdir(db_directory):
         file = db_directory + name
@@ -32,9 +37,30 @@ async def reset():
             
         if(os.path.isdir(file)):
             shutil.rmtree(file)
+
+        for doc in os.listdir(document_directory):
+            file = document_directory + doc
+            if(os.path.isfile(file)):
+                os.remove(file)
+
+            if(os.path.isdir(file)):
+                shutil.rmtree(file)
+        if(os.path.exists('./db')):
+            shutil.rmtree('./db')
             
     return {"success": True, "message":"document and cache has been cleared"}
 
+@app.get("remove-all-files")
+async def removefiles():
+    for doc in os.listdir(document_directory):
+        file = document_directory + doc
+        if(os.path.isfile(file)):
+            os.remove(file)
 
+        if(os.path.isdir(file)):
+            shutil.rmtree(file)
+
+# @app.post("remove-file")
+# async def removefile()
 
     
